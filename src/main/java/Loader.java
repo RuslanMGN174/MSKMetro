66651ca57c66496fcf7d171a62da3573b98e07b5
Vegas <rusland174@gmail.com>
2019-08-09 13:54:03 +0500

    add parser

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,5 +1,26 @@
-public class Loader {
-    public static void main(String[] args) {
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class Loader {
+
+    private static String htmlFile;
+
+    public static void main(String[] args) {
+        htmlFile = parseFile("data/MSKMetro_wikipage.html");
+
+        
+
+    }
+
+    private static String parseFile(String s) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(s));
+            lines.forEach(line -> builder.append(line).append("\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 }
