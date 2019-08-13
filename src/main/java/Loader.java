0bc323d3d6a7aaccfccda8eb9bf1aab32aae5874
Vegas <rusland174@gmail.com>
2019-08-13 23:40:49 +0500

    some experiments for Element and Elements

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,3 +1,10 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -7,9 +14,26 @@ public class Loader {
     private static String htmlFile;
 
     public static void main(String[] args) {
-        htmlFile = parseFile("data/MSKMetro_wikipage.html");
+//        htmlFile = parseFile("data/MSKMetro_wikipage.html");
+        String path = "data/MSKMetro_wikipage.html";
 
-        
+        try {
+            Document document = Jsoup.parse(new File(path), "UTF-8");
+
+            Element table = document.select("table").get(3);
+            Elements rows = table.select("tr");
+
+            for (int i = 0; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                cols.forEach(attr -> System.out.println(attr.text()));
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
