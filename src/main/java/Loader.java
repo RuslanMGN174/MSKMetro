c148407dbda1d58a875d6beb36bb63f4578f7519
Vegas <rusland174@gmail.com>
2019-08-15 00:43:09 +0500

    add some strings

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,17 +1,21 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Loader {
 
     private static String htmlFile;
+    private static PrintWriter pw;
+    private static String newFile = "data/new.csv";
 
     public static void main(String[] args) {
 //        htmlFile = parseFile("data/MSKMetro_wikipage.html");
@@ -19,14 +23,34 @@ public class Loader {
 
         try {
             Document document = Jsoup.parse(new File(path), "UTF-8");
+            pw = new PrintWriter(newFile);
 
             Element table = document.select("table").get(3);
             Elements rows = table.select("tr");
 
-            for (int i = 0; i < rows.size(); i++) {
+            for (int i = 1; i < rows.size(); i++) {
                 Element row = rows.get(i);
                 Elements cols = row.select("td");
-                cols.forEach(attr -> System.out.println(attr.text()));
+
+                String lineColor = getLineColor(cols.get(0).attr("style"));
+                String lineName = cols.get(0).child(1).attr("title");
+                String lineNumber = clearLineNumber(cols.get(0).child(0).text());
+                String stationName = cols.get(1).child(0).text();
+                List<String> connectionLineNumbers = cols.get(3).children().eachText();
+                List<String> connectionLineStations = cols.get(3).children().eachAttr("title");
+
+                //Список станций на линиях
+                Map<String, String> stations = new HashMap<String, String>() {{
+                    put(stationName, lineNumber);
+                }};
+
+//                System.out.println(connectionLineStations);
+//                    System.out.printf("%s %s %s %s %s %s \n", lineNumber, lineName, lineColor, stationName, connectionLineNumbers);
+
+
+//                cols.forEach(attr -> System.out.println(attr
+//                        .children()
+//                        .attr("title")));
             }
 
 
@@ -46,5 +70,14 @@ public class Loader {
             e.printStackTrace();
         }
         return builder.toString();
+    }
+
+    private static String getLineColor(String text) {
+        int first = text.indexOf("#");
+        return text.substring(first, first + 6);
+    }
+
+    private static String clearLineNumber(String number) {
+        return number.length() == 4 ? number.substring(1) : number;
     }
 }
