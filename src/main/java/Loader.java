e660cc8e707d6f211af197b998804027da6cd130
Vegas <rusland174@gmail.com>
2019-08-18 22:43:27 +0500

    first try

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -8,8 +8,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,137 +16,99 @@ import java.util.TreeMap;
 
 public class Loader {
 
-    private static String htmlFile;
-    private static String newFile = "data/new.csv";
-    private static String lineName;
-    private static List<String> lineNumber;
     private static Map<String, List<String>> stations = new TreeMap<>();
-    private static List<String> connectionLineNumbers;
-    private static List<String> connectionLineStations;
-    private static List<String> lines = new ArrayList<>();
-    private static List<List<String>> connections = new ArrayList<>();
 
     private static final JSONArray connectionsJSON = new JSONArray();
     private static final JSONArray linesJSON = new JSONArray();
 
     public static void main(String[] args) {
-//        htmlFile = parseFile("data/MSKMetro_wikipage.html");
         String path = "data/MSKMetro_wikipage.html";
 
         try {
             Document document = Jsoup.parse(new File(path), "UTF-8");
+            parseFile(document.select("table").get(3));
 
-            Element table = document.select("table").get(3);
-            Elements rows = table.select("tr");
+            JSONObject metroStations = parseFile(document.select("table").get(3));
 
-            for (int i = 1; i < rows.size(); i++) {
-                Element row = rows.get(i);
-                Elements cols = row.select("td");
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String metroStationsGson = gson.toJson(metroStations);
 
-                String lineColor = getLineColor(cols.get(0).attr("style"));
-                lineName = cols.get(0).child(1).attr("title");
-                lineNumber = cols.get(0).children().eachText();
-                String stationName = cols.get(1).child(0).text();
-                connectionLineNumbers = cols.get(3).children().eachText();
-                connectionLineStations = cols.get(3).children().eachAttr("title");
+            PrintWriter pw = new PrintWriter("data/MSKMetro.json");
 
+            pw.println(metroStationsGson);
 
-                /*----------------Список переходов на другие станции--------------------*/
-
-                if (connectionLineNumbers.size() != 0) {
-                    connections.add(new ArrayList<String>() {{
-                        for (int j = 0; j < connectionLineNumbers.size(); j++) {
-                            add("line: " + connectionLineNumbers.get(j) + ", " + "station: " + connectionLineStations.get(j));
-                        }
-                    }});
-                }
-
-
-                /*----------------Список станций на линиях-----------------------------*/
-
-                String clearLineNumber = clearLineNumber(lineNumber.get(0));
-                if (!stations.containsKey(clearLineNumber)) {
-                    stations.put(clearLineNumber, new ArrayList<>());
-
-                    //Список линий
-                    lines.add("number: " + clearLineNumber
-                            + " " + "name: " + lineName
-                            + " " + "color: " + lineColor);
-
-                    //Список линий JSON
-                    JSONObject lines = new JSONObject();
-                    lines.put("number", clearLineNumber);
-                    lines.put("name", lineName);
-                    lines.put("color", lineColor);
-
-                    linesJSON.add(lines);
-
-                }
-                if (lineNumber.size() == 2) {
-                    stations.get(clearLineNumber).add(stationName);
-                } else if (lineNumber.size() == 3) {
-                    stations.get(lineNumber.get(1)).add(stationName);
-                    stations.get(clearLineNumber).add(stationName);
-                }
-
-
-
-
-
-                /*----------------Список переходов на другие станции JSON--------------------*/
-                if (connectionLineNumbers.size() != 0) {
-                    JSONArray array = new JSONArray();
-                    JSONObject object = new JSONObject();
-                    object.put("line", lineNumber.get(0));
-                    object.put("station", stationName);
-                    array.add(object);
-
-                    for (int j = 0; j < connectionLineNumbers.size(); j++) {
-                        final JSONObject station2 = new JSONObject();
-                        station2.put("line", connectionLineNumbers.get(j));
-                        station2.put("station", connectionLineStations.get(j));
-                        array.add(station2);
-                    }
-                    connectionsJSON.add(array);
-                }
-            }
+            pw.flush();
+            pw.close();
 
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static JSONObject parseFile(Element element) {
+        Elements rows = element.select("tr");
+
+        for (int i = 1; i < rows.size(); i++) {
+            Element row = rows.get(i);
+            Elements cols = row.select("td");
+
+            String lineColor = getLineColor(cols.get(0).attr("style"));
+            String lineName = cols.get(0).child(1).attr("title");
+            List<String> lineNumber = cols.get(0).children().eachText();
+            String stationName = cols.get(1).text();
+            List<String> connectionLineNumbers = cols.get(3).children().eachText();
+            List<String> connectionLineStations = cols.get(3).children().eachAttr("title");
+
+
+            /*----------------Список станций на линиях-----------------------------*/
+            String clearLineNumber = clearLineNumber(lineNumber.get(0));
+            if (!stations.containsKey(clearLineNumber)) {
+                stations.put(clearLineNumber, new ArrayList<>());
+
+                //Список линий
+                Map<String, String> map = new TreeMap<>();
+                map.put("number", clearLineNumber);
+                map.put("name", lineName);
+                map.put("color", lineColor);
+                JSONObject lines = new JSONObject(map);
+                linesJSON.add(lines);
+
+            }
+            if (lineNumber.size() == 2) {
+                stations.get(clearLineNumber).add(stationName);
+            } else if (lineNumber.size() == 3) {
+                stations.get(lineNumber.get(1)).add(stationName);
+                stations.get(clearLineNumber).add(stationName);
+            }
+
+            /*----------------Список переходов на другие станции--------------------*/
+            if (connectionLineNumbers.size() != 0) {
+                JSONArray array = new JSONArray();
+                JSONObject station1 = new JSONObject();
+                station1.put("line", lineNumber.get(0));
+                station1.put("station", stationName);
+                array.add(station1);
+
+                for (int j = 0; j < connectionLineNumbers.size(); j++) {
+                    final JSONObject station2 = new JSONObject();
+                    station2.put("line", connectionLineNumbers.get(j));
+                    station2.put("station", connectionLineStations.get(j));
+                    array.add(station2);
+                }
+                connectionsJSON.add(array);
+            }
+        }
+
         JSONObject stationsJSON = new JSONObject(stations);
-
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String conn = gson.toJson(connectionsJSON);
-        String lin = gson.toJson(linesJSON);
-        String sta = gson.toJson(stationsJSON);
 
         JSONObject allMetroElements = new JSONObject();
         allMetroElements.put("stations", stationsJSON);
         allMetroElements.put("connections", connectionsJSON);
         allMetroElements.put("lines", linesJSON);
 
-        String all = gson.toJson(allMetroElements);
-
-
-        System.out.println(all);
-
-
-
-    }
-
-    private static String parseFile(String s) {
-        StringBuilder builder = new StringBuilder();
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(s));
-            lines.forEach(line -> builder.append(line).append("\n"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
+        return allMetroElements;
     }
 
     private static String getLineColor(String text) {
